a539a7b24c294c66c0a9955c50ea878c914f6342
KurokuTetsuya <perumnas.good@gmail.com>
2020-09-01 17:20:35 +0700

    fix(auth): Add http server for scan QRcode

--- src/classes/VoidClient.ts
+++ src/classes/VoidClient.ts
@@ -3,14 +3,17 @@ import { resolve } from "path";
 import CommandHandler from "./CommandHandler";
 import EventHandler from "./EventHandler";
 import type { ClientOptions } from "whatsapp-web.js";
+import express from "express";
 
 export default class VoidClient extends Client {
     readonly commandHandler: CommandHandler = new CommandHandler(this, "/", resolve(__dirname, "..", "commands"));
     readonly eventHandler: EventHandler = new EventHandler(this, resolve(__dirname, "..", "events"));
+    readonly http = express();
     public constructor(readonly options: ClientOptions) {
         super(options);
     }
     public async setup(): Promise<void> {
+        this.http.listen(3000);
         this.eventHandler.loadAll();
         this.commandHandler.loadAll();
         return await super.initialize();
--- src/events/QrEvent.ts
+++ src/events/QrEvent.ts
@@ -1,10 +1,17 @@
+/* eslint-disable @typescript-eslint/no-misused-promises */
 import type { EventSchema } from "../interfaces";
 import type VoidClient from "../classes/VoidClient";
+import { toDataURL } from "qrcode";
 
 export default class QrEvent implements EventSchema {
     readonly name = "qr";
     public constructor(readonly client: VoidClient) { }
     public exec(qr: string): void {
-        console.log(`https://api.qrserver.com/v1/create-qr-code/?size=500x500&data=${qr}`);
+        this.client.http.get("/", async (request, response) => {
+            if (this.client.info) return response.status(401).send("<center><h1>Already logged in.</h1></center>");
+            const code = await toDataURL(qr);
+            const body = `<center><img src='${code}' alt="code"></img><br/>Scan this QRCode with Whatsapp on your phone!</center>`;
+            return response.status(200).send(body);
+        });
     }
 }
