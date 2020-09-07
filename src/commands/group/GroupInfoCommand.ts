4249ab92c2a1365a69307eecc0e9150def753b14
Waxaranai <wisnuaa43@gmail.com>
2020-09-07 16:40:29 +0800

    feat(GroupInfoCommand): add group profile pict

--- src/commands/group/GroupInfoCommand.ts
+++ src/commands/group/GroupInfoCommand.ts
@@ -1,5 +1,6 @@
 import Command from "../../classes/Command";
 import { stripIndent } from "common-tags";
+import { MessageMedia } from "whatsapp-web.js";
 import type { Message } from "whatsapp-web.js";
 import type VoidClient from "../../classes/VoidClient";
 
@@ -8,11 +9,23 @@ export default class GroupInfoCommand extends Command {
         super(client, "groupinfo", { content: "Gave group info." }, { category: "group", groupOnly: true, aliases: [] });
     }
     public async exec(msg: Message): Promise<void> {
-        const chat = await msg.getChat();
-        msg.reply(stripIndent(`
-            *Group Info*
+        const chat = await msg.getChat() as any;
+        const url = await this.client.getProfilePicUrl(msg.from);
+        if (url) {
+            const { body: img }: { body: Buffer } = await this.client.request.get(url);
+            msg.reply(new MessageMedia("image/png", img.toString("base64"), "icon.png"), msg.from, {
+                caption: stripIndent(`
+                *Group Info* [${chat.groupMetadata.participants.length} members]
+                - Name: ${chat.name}
+                - ID: ${msg.from}
+            `)
+            });
+        } else {
+            msg.reply(stripIndent(`
+            *Group Info* [${chat.groupMetadata.participants.length} members]
             - Name: ${chat.name}
-            - ID: ${chat.id.user}@${chat.id.server}
-        `));
+            - ID: ${msg.from}
+            `));
+        }
     }
 }
