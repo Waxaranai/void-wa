e9ebedeff47a721737b8439c25c4b98c6ca4c299
Waxaranai <wisnuaa43@gmail.com>
2020-10-16 12:59:51 +0000

    feat(Sticker): add Sticker command

---
+++ src/commands/general/Sticker.ts
@@ -0,0 +1,35 @@
+import type { Client, Message } from "@open-wa/wa-automate";
+import { decryptMedia } from "@open-wa/wa-automate";
+import Command from "../../libs/Command";
+
+export default class StickerCommand extends Command {
+    public constructor(public readonly client: Client) {
+        super("sticker", {
+            aliases: ["stiker"],
+            category: "general",
+            cooldown: 15
+        }, {
+            content: "Create sticker from a image."
+        });
+    }
+
+    public async exec(msg: Message): Promise<void> {
+        const isQuotedImage = msg.quotedMsg && msg.quotedMsg.type === "image";
+        if (msg.isMedia || isQuotedImage) {
+            try {
+                await this.create(msg, isQuotedImage);
+            } catch (error) {
+                await this.client.sendText(msg.chatId, "An error occured while trying to create the sticker!");
+            }
+        } else {
+            await this.client.sendText(msg.chatId, `Please send image with *${this.client.config.prefix}sticker* caption or reply on a image!`);
+        }
+    }
+
+    public async create(message: Message, isQuoted: boolean): Promise<void> {
+        const msg = isQuoted ? message.quotedMsg : message;
+        const media = await decryptMedia(msg, this.client.config.UserAgent);
+        const imageBase64 = `data:${msg.mimetype};base64,${media.toString("base64")}`;
+        await this.client.sendImageAsSticker(message.chatId, imageBase64);
+    }
+}
--- src/config.ts
+++ src/config.ts
@@ -1,3 +1,5 @@
 export const prefix = "$";
 export const port = 5050;
-export default { prefix, port };
+export const UserAgent = "WhatsApp/2.2029.4 Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_5) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/83.0.4103.116 Safari/537.36";
+
+export default { prefix, port, UserAgent };
