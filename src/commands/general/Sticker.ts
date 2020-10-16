4b952e2215ed6e43cde083dc6e3d6bfe9b0b0a51
Waxaranai <wisnuaa43@gmail.com>
2020-10-16 14:45:03 +0000

    feat(Sticker): add to image using flags

--- src/commands/general/Sticker.ts
+++ src/commands/general/Sticker.ts
@@ -13,23 +13,38 @@ export default class StickerCommand extends BaseCommand {
         });
     }
 
-    public async exec(msg: Message): Promise<void> {
+    public async exec(msg: Message, query: string[]): Promise<void> {
+        const { flags } = this.parseArgs(query);
         const isQuotedImage = msg.quotedMsg && msg.quotedMsg.type === "image";
+        const isSticker = msg.quotedMsg && msg.quotedMsg.type === "sticker";
         if (msg.isMedia || isQuotedImage) {
             try {
                 await this.create(msg, isQuotedImage);
             } catch (error) {
                 await this.client.sendText(msg.chatId, "An error occured while trying to create the sticker!");
             }
+        } else if (isSticker && flags.includes("to-image")) {
+            try {
+                await this.create(msg, true, true);
+            } catch (error) {
+                await this.client.sendText(msg.chatId, "An error occured while trying to convert the sticker!");
+            }
+        } else if (!isSticker && flags.includes("to-image")) {
+            await this.client.sendText(msg.chatId, `Please reply a sticker with message *${this.handler!.prefix}sticker --to-image*`);
         } else {
-            await this.client.sendText(msg.chatId, `Please send image with *${this.client.config.prefix}sticker* caption or reply on a image!`);
+            await this.client.sendText(msg.chatId, `Please send image with *${this.handler!.prefix}sticker* caption or reply on a image!`);
         }
     }
 
-    private async create(message: Message, isQuoted: boolean): Promise<void> {
+    private async create(message: Message, isQuoted: boolean, toImage = false): Promise<void> {
         const msg = isQuoted ? message.quotedMsg : message;
         const media = await decryptMedia(msg, this.client.config.UserAgent);
         const imageBase64 = `data:${msg.mimetype};base64,${media.toString("base64")}`;
-        await this.client.sendImageAsSticker(message.chatId, imageBase64);
+        // eslint-disable-next-line no-negated-condition
+        if (!toImage) {
+            await this.client.sendImageAsSticker(message.chatId, imageBase64);
+        } else {
+            await this.client.sendFile(message.chatId, imageBase64, "converted.png", "");
+        }
     }
 }
