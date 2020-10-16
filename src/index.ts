47b5c7a2f63b875fe0f4a2df0458e5b7a0308237
Waxaranai <wisnuaa43@gmail.com>
2020-10-16 12:30:10 +0000

    feat: change qr removing log

--- src/index.ts
+++ src/index.ts
@@ -4,17 +4,18 @@ import server from "./server";
 import { ev, QRFormat, QRQuality } from "@open-wa/wa-automate";
 import { existsSync, mkdirSync, unlinkSync, writeFileSync } from "fs";
 
+const QRExist = existsSync("src/public/qrcode.png");
 ev.on("**", (data, _, namespace) => {
     const scanned = data === "successfulScan" && namespace === "QR";
-    if (scanned) {
+    if (scanned && QRExist) {
         console.log("Client authenticated! removing qrcode image.");
-        if (existsSync("src/public/qrcode.png")) unlinkSync("src/public/qrcode.png");
+        unlinkSync("src/public/qrcode.png");
     }
 });
 ev.on("qr.**", qrcode => {
     console.log(`QRCode received! You can scan it from the terminal or http://localhost:${config.port}/qr`);
     if (!existsSync("src/public")) mkdirSync("src/public");
-    if (existsSync("src/public/qrcode.png")) unlinkSync("src/public/qrcode.png");
+    if (QRExist) unlinkSync("src/public/qrcode.png");
     writeFileSync("src/public/qrcode.png", Buffer.from(
         qrcode.replace("data:image/png;base64,", ""),
         "base64"
