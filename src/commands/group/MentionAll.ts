6b80a4fa126070ed38ffccabfac98c914fe5aba5
Waxaranai <wisnuaa43@gmail.com>
2020-10-28 23:58:37 +0000

    feat: add mentionall command

---
+++ src/commands/group/MentionAll.ts
@@ -0,0 +1,25 @@
+import type { Client, Message } from "@open-wa/wa-automate";
+import BaseCommand from "../../libs/BaseCommand";
+
+export default class MentionAllCommand extends BaseCommand {
+    public constructor(public readonly client: Client) {
+        super("mentionall", {
+            aliases: ["everyone"],
+            category: "group",
+            groupOnly: true,
+            adminOnly: true
+        }, {
+            content: "Mention all group members"
+        });
+    }
+
+    public async exec(msg: Message): Promise<void> {
+        const result: string[] = [];
+        const members = await this.client.getGroupMembers(msg.chatId);
+        for (const member of members) {
+            if (member.isMe) continue;
+            else result.push(`@${(member.id as string).replace(/@c.us/g, "")}`);
+        }
+        await this.client.sendTextWithMentions(msg.chatId, result.join(" "));
+    }
+}
