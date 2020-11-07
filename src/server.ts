7dadce303f0a0e77abeb29328b622c706f3eade7
Waxaranai <wisnuaa43@gmail.com>
2020-11-07 19:56:35 +0000

    fix(ci): fix socket.io problems

--- src/server.ts
+++ src/server.ts
@@ -1,20 +1,20 @@
 import express from "express";
 import http from "http";
-import socket from "socket.io";
 import { existsSync } from "fs";
 import { resolve } from "path";
+import { Server as SocketServer, Socket } from "socket.io";
 
 export default (port: number): void => {
     const app = express();
     const server = http.createServer(app);
-    const io = socket(server);
+    const io = new SocketServer(server);
 
     app.use("/style.css", express.static(resolve("src/public/style.css")));
     app.use("/qrcode.png", express.static(resolve("src/public/qrcode.png")));
     app.set("view engine", "html");
 
 
-    io.on("connection", socket => {
+    io.on("connection", (socket: Socket) => {
         const authenticated = !existsSync("src/public/qrcode.png");
         if (authenticated) return socket.emit("authenticated");
         return socket.emit("qr");
