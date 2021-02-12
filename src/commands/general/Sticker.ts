c87afb0c4bcbe62d88ad16d510969f2f44052adc
Waxaranai <waxaranai@gmail.com>
2021-02-12 23:43:30 +0700

    feat(Sticker): rewrite

--- src/commands/general/Sticker.ts
+++ src/commands/general/Sticker.ts
@@ -7,42 +7,39 @@ import BaseCommand from "../../libs/BaseCommand";
     category: "general",
     cooldown: 30,
     description: {
-        content: "Create sticker from a image. or get the image from a sticker",
-        usage: "[--to-img]"
+        content: "Create sticker from a image or video"
     }
 })
 export default class extends BaseCommand {
-    public async exec(msg: Message, query: string[]): Promise<void> {
-        const { flags } = this.parseArgs(query);
+    public async exec(msg: Message): Promise<void> {
         const isQuotedImage = msg.quotedMsg && msg.quotedMsg.type === "image";
-        const isSticker = msg.quotedMsg && msg.quotedMsg.type === "sticker";
-        if (msg.isMedia || isQuotedImage) {
-            try {
-                await this.create(msg, isQuotedImage);
-            } catch (error) {
-                await this.client.sendText(msg.chatId as any, "An error occured while trying to create the sticker!");
-            }
-        } else if (isSticker && flags.includes("to-img")) {
-            try {
-                await this.create(msg, true, true);
-            } catch (error) {
-                await this.client.sendText(msg.chatId as any, "An error occured while trying to convert the sticker!");
-            }
-        } else if (!isSticker && flags.includes("to-img")) {
-            await this.client.sendText(msg.chatId as any, `Please reply a sticker with message *${this.handler!.prefix}sticker --to-img*`);
+        const isQuotedVideo = msg.quotedMsg && msg.quotedMsg.type === "video";
+        if (msg.isMedia || msg.type === "image" || isQuotedImage) {
+            const wait = await this.client.sendText(msg.chatId as any, "*Please wait...* sometime it takes longer than 3 minutes");
+            await this.create(msg, isQuotedImage);
+            await this.client.deleteMessage(msg.chatId as any, wait as any);
+        } else if (msg.type === "video" || isQuotedVideo) {
+            const wait = await this.client.sendText(msg.chatId as any, "*Please wait...* sometime it takes longer than 3 minutes");
+            await this.create(msg, isQuotedVideo, true);
+            await this.client.deleteMessage(msg.chatId as any, wait as any);
         } else {
-            await this.client.sendText(msg.chatId as any, `Please send image with *${this.handler!.prefix}sticker* caption or reply on a image!`);
+            await this.client.sendText(msg.chatId as any, `Please send image/video/gif with *${this.handler!.prefix}sticker* caption or reply on the file!`);
         }
     }
 
-    private async create(message: Message, isQuoted: boolean, toImage = false): Promise<void> {
-        const msg = isQuoted ? message.quotedMsg : message;
-        const media = await decryptMedia(msg, this.client.config.UserAgent);
-        const imageBase64 = `data:${msg.mimetype};base64,${media.toString("base64")}`;
-        if (!toImage) {
-            await this.client.sendImageAsSticker(message.chatId as any, imageBase64);
-            return undefined;
+    private async create(message: Message, isQuoted: boolean, isGif = false): Promise<void> {
+        try {
+            const msg = isQuoted ? message.quotedMsg : message;
+            const media = await decryptMedia(msg, this.client.config.UserAgent);
+            const imageBase64 = `data:${msg.mimetype};base64,${media.toString("base64")}`;
+            if (isGif) {
+                await this.client.sendMp4AsSticker(message.chatId as any, media.toString("base64"), { crop: false }, { author: "Void", pack: "Created by" });
+                return undefined;
+            }
+            await this.client.sendImageAsSticker(message.chatId as any, imageBase64, { author: "Void", pack: "Created by" });
+        } catch (e) {
+            console.error(e);
+            await this.client.sendText(message.chatId as any, "An error occured when trying to create the sticker.");
         }
-        await this.client.sendFile(message.chatId as any, imageBase64, "converted.png", "");
     }
 }
