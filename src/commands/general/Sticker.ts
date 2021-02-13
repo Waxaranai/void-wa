3206970f6cb84769d29c6225662ac74fc60c5dd2
Waxaranai <waxaranai@gmail.com>
2021-02-13 10:16:54 +0700

    fix(Sticker): fix error handling

--- src/commands/general/Sticker.ts
+++ src/commands/general/Sticker.ts
@@ -15,31 +15,35 @@ export default class extends BaseCommand {
         const isQuotedImage = msg.quotedMsg && msg.quotedMsg.type === "image";
         const isQuotedVideo = msg.quotedMsg && msg.quotedMsg.type === "video";
         if (msg.isMedia || msg.type === "image" || isQuotedImage) {
-            const wait = await this.client.sendText(msg.chatId as any, "*Please wait...* sometime it takes longer than 3 minutes");
-            await this.create(msg, isQuotedImage);
-            await this.client.deleteMessage(msg.chatId as any, wait as any);
+            const wait = await this.client.sendText(msg.chatId as any, "*Please wait...*");
+            await this.create(msg, wait as string, isQuotedImage);
         } else if (msg.type === "video" || isQuotedVideo) {
+            if ((Number(msg.duration) || Number(msg.quotedMsg.duration)) >= 15) {
+                await this.client.sendText(msg.chatId as any, "Please use video/gif with duration under 15 seconds and try again.");
+                return undefined;
+            }
             const wait = await this.client.sendText(msg.chatId as any, "*Please wait...* sometime it takes longer than 3 minutes");
-            await this.create(msg, isQuotedVideo, true);
-            await this.client.deleteMessage(msg.chatId as any, wait as any);
+            await this.create(msg, wait as string, isQuotedVideo, true);
         } else {
             await this.client.sendText(msg.chatId as any, `Please send image/video/gif with *${this.handler!.prefix}sticker* caption or reply on the file!`);
         }
     }
 
-    private async create(message: Message, isQuoted: boolean, isGif = false): Promise<void> {
+    private async create(message: Message, waitMsg: string, isQuoted: boolean, isGif = false): Promise<void> {
         try {
             const msg = isQuoted ? message.quotedMsg : message;
             const media = await decryptMedia(msg, this.client.config.UserAgent);
             const imageBase64 = `data:${msg.mimetype};base64,${media.toString("base64")}`;
             if (isGif) {
                 await this.client.sendMp4AsSticker(message.chatId as any, media.toString("base64"), { crop: false }, { author: "Void", pack: "Created by" });
+                await this.client.deleteMessage(message.chatId as any, waitMsg as any);
                 return undefined;
             }
             await this.client.sendImageAsSticker(message.chatId as any, imageBase64, { author: "Void", pack: "Created by" });
+            await this.client.deleteMessage(message.chatId as any, waitMsg as any);
         } catch (e) {
-            this.client.log.error(e);
-            await this.client.sendText(message.chatId as any, "An error occured when trying to create the sticker.");
+            await this.client.deleteMessage(message.chatId as any, waitMsg as any);
+            await this.client.sendText(message.chatId as any, `An error occured when trying to create the sticker. ${isGif ? "try again with shorter video/gif" : ""}`);
         }
     }
 }
